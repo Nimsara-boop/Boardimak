a3b419ba1291596d16191be426b727b7fa602a73
Pugsy <nimsarakarunaratne@gmail.com>
2025-09-03 23:51:05 +0530

    making profile page for TrustFix

---
+++ boardimak/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Profilepage from './pages/profilepage'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <div className = 'bg-blue-100'> 
+          <h1>User Profile</h1>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ boardimak/src/pages/profilepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profilepage = () => {
+  return (
+    <div className = 'bg-color-bluee-200'> </div>
+  )
+}
+
+export default Profilepage
