d12682437aaa6f10e79f474e244d74bfe0a96b60
Pugsy <nimsarakarunaratne@gmail.com>
2025-09-11 21:18:46 +0530

    adding google maps

---
+++ src/components/googlemapsbutton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GoogleMap, Marker, InfoWindow, useLoadScript } from "@react-google-maps/api";
+
+const mapContainerStyle = { width:"100%", height:"600px"};
+
+const Googlemapsbutton = () => {
+  return (
+    <div>G</div>
+  )
+}
+
+export default Googlemapsbutton
